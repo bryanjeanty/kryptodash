c85c1bec7c18c2149527432e8e269b542f01762f
Bryan Jeanty <bryan.jeanty@outlook.com>
2019-05-06 08:28:49 -0400

    session configuration setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 // load npm packages
 const express = require('express');
+const session = require('express-session');
 const next = require('next');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 // load files
 const { mongooseConfig } = require('./config/mongoose-config.js');
+const { sessionConfig } = require('./config/session-config.js');
 
 // connect to database
 mongoose.connect(process.env.MONGO_URI, mongooseConfig).then(() => console.log('db connected')).catch(error => console.log(`db connection error: ${error.message}`));
@@ -20,6 +22,9 @@ const root = dev ? `http://localhost:${port}` : process.env.PRODUCTION_URL;
 
 // setup next server
 server.prepare().then(() => {
+// setup middleware
+app.use(session(sessionConfig));
+
 // let next handle all next related files and events
 app.get('/_next/*', (request, response) => { handle(request, response) });
 app.get('/static/', (request, response) => { handle(request, response) });
